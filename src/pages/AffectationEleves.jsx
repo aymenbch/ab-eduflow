8e8271a68af1e615e4b5839c4438ba4c333d3470
Aymen BCHINA <aymen.bchina@gmail.com>
2026-03-06 15:05:01 +0000

    File changes

--- src/pages/AffectationEleves.jsx
+++ src/pages/AffectationEleves.jsx
@@ -9,6 +9,10 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent,
+  AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger
+} from "@/components/ui/alert-dialog";
 import StudentCard from "@/components/affectation/StudentCard";
 import StudentAIAssistant from "@/components/affectation/StudentAIAssistant";
 
